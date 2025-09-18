f8d2ab6a3d30102d85feb6c359667039055127ab
Hiba Allah Shakoof <83197133+HibaAsh@users.noreply.github.com>
2025-09-18 21:35:02 +0200

    Update health.get.ts

--- server/routes/health.get.ts
+++ server/routes/health.get.ts
@@ -2,6 +2,8 @@ export default defineEventHandler(() => {
   return { 
     status: 'ok', 
     message: 'Server is running', 
-    timestamp: new Date().toISOString() 
+    timestamp: new Date().toISOString(),
+    url: 'https://nuxt-chat-app-q1mh.onrender.com',
+    socket: 'Available at /socket.io/'
   }
 })
